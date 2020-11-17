bfdc856de87ba6fe1ade6bce3e5b3273d89f7b54
Mohamed Elfawy <melfawy@revampco.com>
2020-11-17 21:01:23 +0200

    add Cin7 api wrapper

---
+++ CIN7Integration/ProductsSync.cs
@@ -0,0 +1,43 @@
+﻿using Cin7ApiWrapper.Common;
+using Cin7ApiWrapper.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIN7Integration
+{
+    public class ProductsSync
+    {
+        #region Fields 
+        public string _CIN7_UsereName { get; set; }
+        public string _CIN7_ApiKey { get; set; }
+        public string _CRM_ApiKey { get; set; }
+        public DateTime? _DateFrom { get; set; }
+
+        #endregion
+        #region Ctor 
+        public ProductsSync(string cinUserName, string cinAPiKey, string crmAPIKey, DateTime? fromDate)
+        {
+            this._CIN7_ApiKey = cinAPiKey;
+            this._CIN7_UsereName = cinUserName;
+            this._CRM_ApiKey = crmAPIKey;
+            this._DateFrom = fromDate;
+        }
+        #endregion
+        #region Functions 
+        public void Start()
+        {
+            //1- get data from Cin7
+            var api = new Cin7Api(new ApiUser(this._CIN7_UsereName, this._CIN7_ApiKey));
+
+
+
+            // 2- post data to CRM
+
+        }
+        #endregion
+
+    }
+}
--- CIN7Integration/StartUp.cs
+++ CIN7Integration/StartUp.cs
@@ -8,5 +8,26 @@ namespace CIN7Integration
 {
     public class StartUp
     {
+        public bool StartSync(string cinUserName,string cinAPiKey ,string crmAPIKey , DateTime? fromDate)
+        {
+            try
+            {
+                var product = new ProductsSync(cinUserName, cinAPiKey, crmAPIKey, fromDate);
+                var category = new CategoriesSync(cinUserName, cinAPiKey, crmAPIKey, fromDate);
+                var contact = new ContactsSync(cinUserName, cinAPiKey, crmAPIKey, fromDate);
+                var order = new OrderSync(cinUserName, cinAPiKey, crmAPIKey, fromDate);
+
+                category.Start();
+                product.Start();
+                contact.Start();
+                order.Start();
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
     }
 }
