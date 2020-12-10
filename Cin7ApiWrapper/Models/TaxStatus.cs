62ab0c210f4668b2019dd81348f2053a575a4a5f
Mohamed Elfawy <melfawy@revampco.com>
2020-12-10 03:30:35 +0200

    comit

---
+++ Cin7ApiWrapper/Models/TaxStatus.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Cin7ApiWrapper.Models
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TaxStatus
+    {
+        [EnumMember(Value = "Incl")]
+        Including,
+        [EnumMember(Value = "Excl")]
+        Excluding,
+        [EnumMember(Value = "Exempt")]
+        Exempt
+    }
+}
